1e0d20ee45c92c80b789dcd331ecaca136f3e7f8
Léo Bergeaud <bergeaudleo@gmail.com>
2025-12-02 15:50:42 +0100

    TP4 : ajout film image

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,60 @@ export default function App() {
   console.log(moviesList);
   return (
     <>
-      <h1 className={styles.title}>CinéTech</h1>
-      <main>
-        <h2>Films à l'affiche</h2>
-        <ul>
+      <h1 className="text-4xl font-bold text-center my-6">CinéTech</h1>
+      <main className="max-w-7xl mx-auto mt-6">
+        <h2 className="text-2xl font-bold mb-4">Films à l'affiche</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-5 gap-6">
           {moviesList.map((movie) => (
-            <li key={movie.id}>
+            <li key={movie.id} className="bg-gray-100 rounded-xl p-4 shadow">
               <h3>{movie.title}</h3>
               <p>
                 {movie.year} — {movie.director}
               </p>
               <p>Note : {movie.rating}</p>
+              <img
+                src={movie.poster}
+                alt={movie.title}
+                className="w-full h-64 object-cover rounded"
+              />
             </li>
           ))}
         </ul>
-        <h2>Coups de coeur</h2>
-        <ul>
+        <h2 className="text-2xl font-bold mb-4">Coups de coeur</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-5 gap-6">
           {moviesList
             .filter((movie) => movie.rating >= 8)
             .map((movie) => (
-              <li key={movie.id}>
+              <li key={movie.id} className="bg-gray-100 rounded-xl p-4 shadow">
                 <h3>{movie.title}</h3>
                 <p>
                   {movie.year} — {movie.director}
                 </p>
                 <p>Note : {movie.rating}</p>
+                <img
+                  src={movie.poster}
+                  alt={movie.title}
+                  className="w-full h-64 object-cover rounded"
+                />
               </li>
             ))}
         </ul>
-        <h2>Films de super-héros</h2>
-        <ul>
+        <h2 className="text-2xl font-bold mb-4">Films de super-héros</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-5 gap-6">
           {moviesList
             .filter((movie) => movie.categorie == 'Super-héros')
             .map((movie) => (
-              <li key={movie.id}>
+              <li key={movie.id} className="bg-gray-100 rounded-xl p-4 shadow">
                 <h3>{movie.title}</h3>
                 <p>
                   {movie.year} — {movie.director}
                 </p>
                 <p>Note : {movie.rating}</p>
+                <img
+                  src={movie.poster}
+                  alt={movie.title}
+                  className="w-full h-64 object-cover rounded"
+                />
               </li>
             ))}
         </ul>
