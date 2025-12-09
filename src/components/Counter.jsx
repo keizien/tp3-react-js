31d70f2d1b2fae010ecae3378fbc7f12dd7e2799
Léo Bergeaud <bergeaudleo@gmail.com>
2025-12-09 14:15:47 +0100

    TP6 : decouverte du useState – compteur

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export default function Counter({ valeurInitiale = 0, change = 1 }) {
+  const [count, setCount] = useState(valeurInitiale);
   const increment = () => {
-    setCount(count + 1);
+    setCount(count + change);
   };
   const decrement = () => {
-    setCount(count - 1);
+    setCount(count - change);
+  };
+  const reinitialise = () => {
+    setCount(valeurInitiale);
   };
   return (
     <div className="bg-gray-100 rounded-xl p-4 shadow">
@@ -18,10 +21,16 @@ export default function Counter() {
         +1
       </button>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-1 rounded"
+        className="bg-red-500 hover:bg-red-700 text-white font-bold px-2 py-1 rounded"
         onClick={decrement}
       >
         -1
+      </button>
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold px-2 py-1 rounded"
+        onClick={reinitialise}
+      >
+        Réinitialiser
       </button>
     </div>
   );
